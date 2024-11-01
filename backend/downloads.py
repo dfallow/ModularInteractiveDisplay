e347e5bc875144cb066e37691e3f29007726010f
David Fallow <dfallow@protonmail.com>
2024-11-01 09:23:37 +0200

    added openai key to .env, accessed it in downloads.py

--- backend/downloads.py
+++ backend/downloads.py
@@ -7,17 +7,13 @@ import os
 import PyPDF2
 from markupsafe import escape
 from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
 
-#url = "https://www.theseus.fi/bitstream/handle/10024/506961/Vainola_Lassi.pdf?sequence=2&isAllowed=y"
-
-api_key = "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"
-
-#file_path = 'backend/files/thesis.pdf'
-#urllib.request.urlretrieve(url, file_path)  
+load_dotenv()
 
 # OpenAI API Client
 client = OpenAI(
-    api_key=api_key,
+    api_key=os.getenv("OPENAI_KEY")
 )
 
 # Download thesis pdf file from url
@@ -49,13 +45,8 @@ def ask_question(text, question):
     )
     return response.choices[0].message.content
 
-#pdf_path = 'backend/files/thesis.pdf'
-#pdf_text = extract_text_from_pdf(pdf_path)
-
 # The Question that will be asked to AI
 question = "Create 3 short bullet points (max 80 characters per point) for this thesis that will make a user interested in reading it."
-
-# https://www.theseus.fi/bitstream/handle/10024/506961/Vainola_Lassi.pdf?sequence=2&isAllowed=y
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -80,6 +71,5 @@ def download():
         return ({'result': 'No file provided'})
     
     
- 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
